9c03a1a541c0fb05781abced2d16738b68b429c0
Steve <steve@kitkat@gmail.com>
2024-07-11 01:46:32 +0700

    Formatter to 5 decimal places

--- presentation/src/main/java/me/androidbox/presentation/utils/Utilities.kt
+++ presentation/src/main/java/me/androidbox/presentation/utils/Utilities.kt
@@ -3,7 +3,7 @@ package me.androidbox.presentation.utils
 import java.text.DecimalFormat
 
 fun String.toFormattedPrice(): String {
-    val formatter = DecimalFormat("#,##0.000000")
+    val formatter = DecimalFormat("#,##0.00000")
 
     return this.toDoubleOrNull()?.let { price ->
         return formatter.format(price)
